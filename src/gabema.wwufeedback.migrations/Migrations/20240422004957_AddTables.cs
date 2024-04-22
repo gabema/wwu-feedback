f8a3997741846d1e9a492f053ddce2cb98e26e56
Gabe Martin <gabema@gmail.com>
2024-04-21 17:54:02 -0700

    Updating initial DB Schema

--- src/gabema.wwufeedback.migrations/Migrations/20240422004957_AddTables.cs
+++ src/gabema.wwufeedback.migrations/Migrations/20240422004957_AddTables.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -30,7 +31,9 @@ namespace gabema.wwufeedback.migrations.Migrations
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
                     TopicId = table.Column<int>(type: "int", nullable: false),
-                    Rating = table.Column<short>(type: "smallint", nullable: false)
+                    Rating = table.Column<short>(type: "smallint", nullable: false),
+                    LastUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Feedback = table.Column<string>(type: "nvarchar(max)", nullable: false)
                 },
                 constraints: table =>
                 {
--- src/gabema.wwufeedback.migrations/Models/Vote.cs
+++ src/gabema.wwufeedback.migrations/Models/Vote.cs
@@ -5,4 +5,9 @@ public class Vote
     public int Id { get; set;}
     public int TopicId { get; set;}
     public short Rating { get; set;}
+
+    // Always set using GMT
+    public DateTime LastUpdated { get; set;}
+
+    public string Feedback { get; set; } = string.Empty;
 }
